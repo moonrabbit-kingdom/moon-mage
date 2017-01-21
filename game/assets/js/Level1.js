53e4590ad7b8889e5af011e9aa39e3c591312dfa
Spencer Apple <splayemu@gmail.com>
2017-01-21 12:29:45 -0800

    Add player object and movement

--- game/assets/js/Level1.js
+++ game/assets/js/Level1.js
@@ -17,20 +17,8 @@ MoonMage.Level1.prototype = {
 
         this.game.physics.startSystem(Phaser.Physics.ARCADE);
 
-        // The player and its settings
-        this.player = this.game.add.sprite(32, this.game.world.height - 150, 'dude');
+        this.player = new MoonMage.entities.player(this.game);
 
-        //  We need to enable physics on the player
-        this.game.physics.arcade.enable(this.player);
-
-        //  Player physics properties. Give the little guy a slight bounce.
-        this.player.body.bounce.y = 0.2;
-        this.player.body.gravity.y = 300;
-        this.player.body.collideWorldBounds = true;
-
-        //  Our two animations, walking left and right.
-        this.player.animations.add('left', [0, 1, 2, 3], 10, true);
-        this.player.animations.add('right', [5, 6, 7, 8], 10, true);
     },
 
     buildWorld: function () {
@@ -49,10 +37,8 @@ MoonMage.Level1.prototype = {
     },
 
     update: function() {
-        var hitPlatform = this.game.physics.arcade.collide(this.player, this.platformGroup);
-        console.log('hitPlatform', hitPlatform);
+        var hitPlatform = this.game.physics.arcade.collide(this.player.sprite, this.platformGroup);
+
+        this.player.update(hitPlatform);
     },
-
-
-
 }
