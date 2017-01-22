5bfbcffee903992fb4fc6479678ecf0f3cf9004f
Spencer Apple <splayemu@gmail.com>
2017-01-22 00:22:16 -0800

    Added drag to boxes

--- game/assets/js/entities/Box.js
+++ game/assets/js/entities/Box.js
@@ -4,13 +4,8 @@ MoonMage.entities.Box = function (game, x, y, spriteName) {
     game.physics.arcade.enable(this);
     console.log(this);
     this.body.gravity.y = 600;
+    this.body.drag.x = 300;
 }
 
 MoonMage.entities.Box.prototype = Object.create(Phaser.Sprite.prototype);
 MoonMage.entities.Box.prototype.constructor = MoonMage.entities.Box;
-
-MoonMage.entities.Box.prototype.update = function () {
-   if (this.body.y < (this.game.height - 50)) {
-       //console.log (this.body.y);
-   }
-}
