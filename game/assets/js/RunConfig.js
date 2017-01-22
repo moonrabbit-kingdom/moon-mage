75b9cc54a55fbc531b0a443c4b91ee5a4c8295ac
Thomas Johnston <trjohnst211@gmail.com>
2017-01-22 01:05:26 -0800

    Adding startState to run config to quickly boot into a state

--- game/assets/js/RunConfig.js
+++ game/assets/js/RunConfig.js
@@ -1,8 +1,24 @@
 MoonMage.config = {
+    /**
+     * Use quickboot to quickly jump into a state, simply provide a string with the
+     * registered state name
+     *
+     * If none is provided the game will load into the MainMenu
+     */
+    startState: null,
+
+    /**
+     * Viewport dimensions
+     */
     viewport: {
         height: 562,
         width: 1000
     },
+
+    /**
+     * Used in conjunction with MoonMage.debug to define channels to enable debugging on
+     * When a flag is enabled, all debug prints on that channel will print to the console
+     */
     debug: {
         stateHooks: false
     }
