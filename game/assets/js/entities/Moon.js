1068bbd1842f6d67d37af3402cfcae9c72e18572
Thomas Johnston <trjohnst211@gmail.com>
2017-02-11 16:10:54 -0800

    Keeping moon and wave visible aligned on x-axis

--- game/assets/js/entities/Moon.js
+++ game/assets/js/entities/Moon.js
@@ -5,10 +5,11 @@ MoonMage.entities.Moon = function(game) {
     this.defaultY = 60;
 
     this.moonRender = this.game.add.sprite(this.defaultX, this.defaultY, "moon");
+    this.halfWidth = this.moonRender.width / 2;
 };
 
 MoonMage.entities.Moon.prototype = {
     update: function(x) {
-        this.moonRender.position.x = x;
+        this.moonRender.position.x = x - this.halfWidth;
     }
 };
